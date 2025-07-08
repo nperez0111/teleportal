9f6817ab4ffc7f631599cf280dbc7301d2911be9
Nick the Sick <computers@nickthesick.com>
2025-07-08 08:04:19 +0200

    fix: clean up mobile styles

--- playground/src/components/editor.tsx
+++ playground/src/components/editor.tsx
@@ -35,12 +35,15 @@ export function Editor({ provider, user }: EditorProps) {
       : undefined,
     domAttributes: {
       editor: {
-        class: "flex-1 w-full",
+        class: "flex-1 w-full min-h-full",
+        style: "min-height: calc(100vh - 200px);",
       },
     },
   });
 
   return (
-    <BlockNoteView editor={editor} className="h-full w-full flex flex-col" />
+    <div className="h-full w-full flex flex-col touch-manipulation">
+      <BlockNoteView editor={editor} className="h-full w-full flex flex-col" />
+    </div>
   );
 }
